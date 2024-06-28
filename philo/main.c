4fd594f19836f22a52684f86d3dfd56fb5b77629
atormac <atorma@student.hive.fi>
2024-06-28 16:48:28 +0300

    optional eat_count argument handle

--- philo/main.c
+++ philo/main.c
@@ -6,13 +6,13 @@
 /*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 13:57:06 by atorma            #+#    #+#             */
-/*   Updated: 2024/06/28 14:27:43 by atorma           ###   ########.fr       */
+/*   Updated: 2024/06/28 16:48:09 by atorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	parse_args(t_philo *p, char **argv)
+int	parse_args(t_philo *p, int argc, char **argv)
 {
 	int	error;
 
@@ -21,6 +21,13 @@ int	parse_args(t_philo *p, char **argv)
 	p->time_die = ft_atoi(argv[2], &error);
 	p->time_eat = ft_atoi(argv[3], &error);	
 	p->time_sleep = ft_atoi(argv[4], &error);
+	p->is_opt = 0;
+	p->count_eat = 1;
+	if (argc == 6)
+	{
+		p->count_eat = ft_atoi(argv[5], &error);
+		p->is_opt = 1;
+	}
 	if (error)
 		return (0);
 
@@ -28,6 +35,7 @@ int	parse_args(t_philo *p, char **argv)
 	printf("p->time_die: %zu\n", p->time_die);
 	printf("p->time_eat: %zu\n", p->time_eat);
 	printf("p->time_sleep: %zu\n", p->time_sleep);
+	printf("p->eat_count: %zu\n", p->count_eat);
 	return (1);
 }
 
@@ -35,12 +43,12 @@ int main(int argc, char **argv)
 {
 	t_philo	p;
 
-	if (argc != 5)
+	if (argc != 5 && argc != 6)
 	{
-		printf("Usage: ./philo <number_philos> <die_ms> <eat_ms> <sleep_ms>\n");
+		printf("Usage: ./philo <number_philos> <die_ms> <eat_ms> <sleep_ms>, <opt_eat_count>\n");
 		return (0);
 	}
-	if (!parse_args(&p, argv))
+	if (!parse_args(&p, argc, argv))
 	{
 		printf("Error: Invalid arguments\n");
 		return (0);
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 14:09:48 by atorma            #+#    #+#             */
-/*   Updated: 2024/06/28 14:28:01 by atorma           ###   ########.fr       */
+/*   Updated: 2024/06/28 16:44:46 by atorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ typedef struct t_philo
 	size_t	time_die;
 	size_t	time_eat;
 	size_t	time_sleep;
+	size_t	count_eat;
+	int		is_opt;
 }	t_philo;
 
 size_t	ft_atoi(const char *s, int *error);
